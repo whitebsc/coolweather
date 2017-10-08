0d910b8f544b8d337961cae9ea37418ab83a7fd4
white <baishucheng@gmail.com>
2017-10-08 19:26:50 +0800

    增加后台自动更新

--- app/src/main/java/android/coolweather/com/coolweather/WeatherActivity.java
+++ app/src/main/java/android/coolweather/com/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package android.coolweather.com.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.coolweather.com.coolweather.gson.Forecast;
 import android.coolweather.com.coolweather.gson.Weather;
@@ -219,6 +220,8 @@ public class WeatherActivity extends AppCompatActivity {
 
         weatherLayout.setVisibility(View.VISIBLE);
 
+        Intent intent = new Intent(this,AutoUpdateService.class);
+        startService(intent);
 
 
 
